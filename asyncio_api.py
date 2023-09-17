9ff07dc589f22a22d4baf0be29fcade326a640e6
TannyKorry <tannykorry@gmail.com>
2023-09-17 18:18:31 +0300

    отработать код, если по id нет данных

--- asyncio_api.py
+++ asyncio_api.py
@@ -2,22 +2,85 @@ import asyncio
 import aiohttp
 import datetime
 from pprint import pprint
+
+import requests
+
 from models import Base, SwapiPeople, Session, engine
 from more_itertools import chunked
 
-MAX_CHUNK_SIZE = 10
+
+
+MAX_CHUNK_SIZE = 5
+
+
 # Выгрузка персонажей из SWAPI
 async def get_people(people_id):
     session = aiohttp.ClientSession()
     response = await session.get(f"https://swapi.dev/api/people/{people_id}")
     json_data = await response.json()
+
+    if 'detail' not in json_data:
+
+        homeworld_url = json_data['homeworld']
+        async with session.get(f'{homeworld_url}') as resp:
+            json_item = await resp.json()
+            homeworld = json_item['name']
+
+        films_list, species_list, starships_list, vehicles_list = [], [], [], []
+
+        for film in json_data['films']:
+            async with session.get(f'{film}') as resp:
+                json_item = await resp.json()
+                films_list.append(json_item['title'])
+
+        for species in json_data['species']:
+            async with session.get(f'{species}') as resp:
+                json_species = await resp.json()
+                species_list.append(json_species['name'])
+
+        for starship in json_data['starships']:
+            async with session.get(f'{starship}') as resp:
+                json_starships = await resp.json()
+                starships_list.append(json_starships['name'])
+
+        for vehicle in json_data['vehicles']:
+            async with session.get(f'{vehicle}') as resp:
+                json_vehicles = await resp.json()
+                vehicles_list.append(json_vehicles['name'])
+
+        add_data = {
+            'id': people_id,
+            'films': ",".join(films_list),
+            'species': ",".join(species_list),
+            'starships': ",".join(starships_list),
+            'vehicles': ",".join(vehicles_list),
+            'homeworld': homeworld,
+        }
+        json_data.update(add_data)
     await session.close()
     return json_data
 
 
 async def insert_to_db(people_json_list):
+    # pprint(people_json_list)
     async with Session() as session:
-        swapi_people_list = [SwapiPeople(json=json_data) for json_data in people_json_list]
+
+        swapi_people_list = [SwapiPeople(
+            name=json_data['name'],
+            person_id=json_data['id'],
+            birth_year=json_data['birth_year'],
+            eye_color=json_data['eye_color'],
+            films=json_data['films'],
+            gender=json_data['gender'],
+            hair_color=json_data['hair_color'],
+            height=json_data['height'],
+            homeworld=json_data['homeworld'],
+            mass=json_data['mass'],
+            skin_color=json_data['skin_color'],
+            species=json_data['species'],
+            starships=json_data['starships'],
+            vehicles=json_data['vehicles'],
+        ) for json_data in people_json_list]
         session.add_all(swapi_people_list)
         await session.commit()
 
@@ -26,9 +89,8 @@ async def main():
     async with engine.begin() as con:
         await con.run_sync(Base.metadata.create_all)
 
+    for ids_chunk in chunked(range(1, 100), MAX_CHUNK_SIZE):
 
-    # q = len(requests.get(f"https://swapi.dev/api/people").json()['results'])
-    for ids_chunk in chunked(range(1, 84), MAX_CHUNK_SIZE):
         coros = [get_people(people_id) for people_id in ids_chunk]
         people_json_list = await asyncio.gather(*coros)
         asyncio.create_task(insert_to_db(people_json_list)) #вместо await, чтобы вставка в базу не блокировала получение следующего персонажа, будем использовать задачу
@@ -42,3 +104,4 @@ async def main():
 start = datetime.datetime.now()
 asyncio.run(main())
 pprint(datetime.datetime.now() - start)
+
--- models.py
+++ models.py
@@ -1,15 +1,15 @@
 import os
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy import JSON, Column, DateTime, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 PG_USER = os.getenv("PG_USER", "user")
 PG_PASSWORD = os.getenv("PG_PASSWORD", "123")
-PG_DB = os.getenv("PG_DB", "asy")
+PG_DB = os.getenv("PG_DB", "asyncio")
 PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
-PG_PORT = os.getenv("PG_PORT", 5431)
+PG_PORT = os.getenv("PG_PORT", 5435)
 
 PG_DSN = f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
 engine = create_async_engine(PG_DSN)
@@ -24,4 +24,17 @@ class SwapiPeople(Base):
     __tablename__ = "Heroes"
 
     id = Column(Integer, primary_key=True)
-    json =Column(JSON)
+    person_id = Column(Integer)
+    name = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
+    films = Column(String)
+    gender = Column(String)
+    hair_color = Column(String)
+    height = Column(String)
+    homeworld = Column(String)
+    mass = Column(String)
+    skin_color = Column(String)
+    species = Column(String)
+    starships = Column(String)
+    vehicles = Column(String)
